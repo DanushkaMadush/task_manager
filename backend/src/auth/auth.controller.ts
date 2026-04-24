18761b30c61ba3837829fb127f0a6854c3f77568
danushka <danmadushzxc@gmail.com>
2026-04-24 18:44:48 +0530

    refactor:changed response code

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from 'src/dto/login.dto';
 
@@ -8,6 +8,7 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('login')
+    @HttpCode(200)
     login(@Body() dto: LoginDto) {
         return this.authService.login(dto);
     }
