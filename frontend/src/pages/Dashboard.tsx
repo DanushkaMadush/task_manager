3a4d0a5670f825d3f6abfdd66653a922b0eeeae9
danushka <danmadushzxc@gmail.com>
2026-04-24 20:34:32 +0530

    feat:initial pages

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { login } from "../api/services/auth.service";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await login({ username, password });
+      localStorage.setItem("token", res.access_token);
+      window.location.href = "/tasks";
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="p-6 bg-white shadow rounded w-80">
+        <h2 className="text-xl mb-4">Login</h2>
+
+        <input
+          className="border w-full mb-2 p-2"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          className="border w-full mb-4 p-2"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          className="bg-blue-500 text-white w-full p-2 rounded"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
